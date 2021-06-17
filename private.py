4fc308892a9e5f8644d6b59832190006563b93e1
Himanshu-kr-007 <69851575+Himanshu-kr-007@users.noreply.github.com>
2021-06-17 16:12:15 +0530

    Add files via upload

---
+++ private.py
@@ -0,0 +1,7 @@
+ReceiverEmail = "###############" #change This
+EmailTitle = "Hello! Sir," #change This
+EmailBody = "How are you ?" #change This
+num = f"+91 ########" #change This
+msg = "H###############" #change This
+uname = "#############" #change This
+passwd = "#############" #change This
